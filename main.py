d9ac34f83c5b4dbd3c39d3007f8907a58e9e41ae
Marcel Mertens <marcel.mertens@me.com>
2023-11-20 15:00:33 +0100

    Full Rework

--- main.py
+++ main.py
@@ -43,13 +43,39 @@ def get_config(configname):
 
 
 
-async def connect():
-    if DEVICE_TOKEN and DEVICE_KEY:
-        await device.authenticate(DEVICE_TOKEN, DEVICE_KEY)
-        
+# Build FastAPI app
+app = FastAPI()
 
-def status():
-    status =  {
+
+# Show known devices
+@app.get("/devices")
+async def show_devices():
+    return config_parser.sections()
+
+# Show Configs
+@app.get("/{device}/config")
+async def read_config(device):
+    cfg = get_config(device)
+    return cfg
+
+
+# Show Status of AC Unit
+@app.get('/{device}/status')
+async def ac_status(device):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # build status dict.
+        values =  {
         'id': device.id,
         'ip': device.ip,
         'online': device.online,
@@ -66,102 +92,230 @@ def status():
         'indoor_temperature': device.indoor_temperature,
         'outdoor_temperature': device.outdoor_temperature
         }
-    return status
-
-def get_capabilities():
-    values = {
-        'supported_modes': repr(device.supported_operation_modes),
-        'supported_swing_modes': repr(device.supported_swing_modes),
-        'supported_fan_speeds': repr(device.supported_fan_speeds),
-        'supports_eco_mode': device.supports_eco_mode,
-        'supports_turbo_mode': device.supports_turbo_mode,
-        'max_target_temperature': device.max_target_temperature,
-        'min_target_temperature': device.min_target_temperature
-        }
-    return values
-
-def config_data():
-    values = {
-        'name': deviceconfig["NAME"],
-        'modul': MODUL,
-        'device-ip': DEVICE_IP,
-        'device-port': DEVICE_PORT,
-        'device-id': DEVICE_ID,
-        'device-key': DEVICE_KEY,
-        'device-token': DEVICE_TOKEN
-    }
-    return values
+        return values
 
 
-app = FastAPI()
 
-
-@app.get("/{name}/config")
-async def read_config(name):
-    cfg = get_config(name)
-    return cfg
-
-
-#Show Status of AC Unit
-@app.get('/status')
-async def ac_status():
-    await connect()
-    await device.refresh()
-    return status()
-
-
-#Show Capabilites (Mode, Fan Speed, Swing Mode etc) of AC Unit
-@app.get('/capabilities')
-async def capabilities():
-    await connect()
-    await device.get_capabilities()
-    return get_capabilities()
+# Show Capabilites (Mode, Fan Speed, Swing Mode etc) of AC Unit
+@app.get('/{device}/capabilities')
+async def capabilities(device):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device capabilities
+        await device.get_capabilities()
+        # build capabilities dict.
+        values = {
+            'supported_modes': repr(device.supported_operation_modes),
+            'supported_swing_modes': repr(device.supported_swing_modes),
+            'supported_fan_speeds': repr(device.supported_fan_speeds),
+            'supports_eco_mode': device.supports_eco_mode,
+            'supports_turbo_mode': device.supports_turbo_mode,
+            'max_target_temperature': device.max_target_temperature,
+            'min_target_temperature': device.min_target_temperature
+            }
+        return values
 
 
 # Turn AC Unit Off
-@app.post('/poweroff')
-async def ac_poweroff():
-    await connect()
-    await device.refresh()
-    device.power_state = False
-    await device.apply()
-    return status()
+@app.post('/{device}/poweroff')
+async def ac_poweroff(device):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # set power state = false
+        device.power_state = False
+        # apply status to device
+        await device.apply()
+         # build status dict.
+        values =  {
+        'id': device.id,
+        'ip': device.ip,
+        'online': device.online,
+        'supported': device.supported,
+        'power_state': device.power_state,
+        'beep': device.beep,
+        'target_temperature': device.target_temperature,
+        'operational_mode': repr(device.operational_mode),
+        'fan_speed': repr(device.fan_speed),
+        'swing_mode': repr(device.swing_mode),
+        'eco_mode': device.eco_mode,
+        'turbo_mode': device.turbo_mode,
+        'fahrenheit': device.fahrenheit,
+        'indoor_temperature': device.indoor_temperature,
+        'outdoor_temperature': device.outdoor_temperature
+        }
+        return values
+
 
 # Turn AC Unit On
-@app.post('/poweron')
-async def ac_poweron():
-    await connect()
-    await device.refresh()
-    device.power_state = True
-    await device.apply()
-    return status()
+@app.post('/{device}/poweron')
+async def ac_poweron(device):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # set power state = True
+        device.power_state = True
+        # apply status to device
+        await device.apply()
+         # build status dict.
+        values =  {
+        'id': device.id,
+        'ip': device.ip,
+        'online': device.online,
+        'supported': device.supported,
+        'power_state': device.power_state,
+        'beep': device.beep,
+        'target_temperature': device.target_temperature,
+        'operational_mode': repr(device.operational_mode),
+        'fan_speed': repr(device.fan_speed),
+        'swing_mode': repr(device.swing_mode),
+        'eco_mode': device.eco_mode,
+        'turbo_mode': device.turbo_mode,
+        'fahrenheit': device.fahrenheit,
+        'indoor_temperature': device.indoor_temperature,
+        'outdoor_temperature': device.outdoor_temperature
+        }
+        return values
 
 # Set Temperature
-@app.post('/temperature/{temperature}')
-async def ac_temperature(temperature: int):
-    await connect()
-    await device.refresh()
-    device.target_temperature = temperature
-    await device.apply()
-    return status()
+@app.post('/{device}/temperature/{temperature}')
+async def ac_temperature(device, temperature: int):
+    
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # set target temperature
+        device.target_temperature = temperature
+        # apply status to device
+        await device.apply()
+         # build status dict.
+        values =  {
+        'id': device.id,
+        'ip': device.ip,
+        'online': device.online,
+        'supported': device.supported,
+        'power_state': device.power_state,
+        'beep': device.beep,
+        'target_temperature': device.target_temperature,
+        'operational_mode': repr(device.operational_mode),
+        'fan_speed': repr(device.fan_speed),
+        'swing_mode': repr(device.swing_mode),
+        'eco_mode': device.eco_mode,
+        'turbo_mode': device.turbo_mode,
+        'fahrenheit': device.fahrenheit,
+        'indoor_temperature': device.indoor_temperature,
+        'outdoor_temperature': device.outdoor_temperature
+        }
+        return values
 
 # Set Operation Mode
-@app.post('/operationmode/{operationmode}')
-async def ac_operationmode(operationmode: int):
-    await connect()
-    await device.refresh()
-    device.operational_mode = operationmode
-    await device.apply()
-    return status()
+@app.post('/{device}/operationmode/{operationmode}')
+async def ac_operationmode(device, operationmode: int):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # set operational_mode
+        device.operational_mode = operationmode
+        # apply status to device
+        await device.apply()
+         # build status dict.
+        values =  {
+        'id': device.id,
+        'ip': device.ip,
+        'online': device.online,
+        'supported': device.supported,
+        'power_state': device.power_state,
+        'beep': device.beep,
+        'target_temperature': device.target_temperature,
+        'operational_mode': repr(device.operational_mode),
+        'fan_speed': repr(device.fan_speed),
+        'swing_mode': repr(device.swing_mode),
+        'eco_mode': device.eco_mode,
+        'turbo_mode': device.turbo_mode,
+        'fahrenheit': device.fahrenheit,
+        'indoor_temperature': device.indoor_temperature,
+        'outdoor_temperature': device.outdoor_temperature
+        }
+        return values
 
 # Set Fan Speed
-@app.post('/fanspeed/{fanspeed}')
-async def ac_fanspeed(fanspeed: int):
-    await connect()
-    await device.refresh()
-    device.fanspeed = fanspeed
-    await device.apply()
-    return status()    
+@app.post('/{device}/operationmode/{fanspeed}')
+async def ac_fanspeed(device, fanspeed: int):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # set operational_mode
+        device.fan_speed = fanspeed
+        # apply status to device
+        await device.apply()
+         # build status dict.
+        values =  {
+        'id': device.id,
+        'ip': device.ip,
+        'online': device.online,
+        'supported': device.supported,
+        'power_state': device.power_state,
+        'beep': device.beep,
+        'target_temperature': device.target_temperature,
+        'operational_mode': repr(device.operational_mode),
+        'fan_speed': repr(device.fan_speed),
+        'swing_mode': repr(device.swing_mode),
+        'eco_mode': device.eco_mode,
+        'turbo_mode': device.turbo_mode,
+        'fahrenheit': device.fahrenheit,
+        'indoor_temperature': device.indoor_temperature,
+        'outdoor_temperature': device.outdoor_temperature
+        }
+        return values 
 
 
 class ac_config(BaseModel):
@@ -173,19 +327,46 @@ class ac_config(BaseModel):
     eco_mode: Optional[bool] = None
     turbo_mode: Optional[bool] = None
     
-@app.post('/bulk')
-async def parameter_data(s1: ac_config):
-    await connect()
-    await device.refresh()
-    device.power_state = s1.power_state
-    device.target_temperature = s1.target_temperature
-    device.operational_mode = s1.operational_mode
-    device.fan_speed = s1.fan_speed
-    device.swing_mode = s1.swing_mode
-    device.eco_mode = s1.eco_mode
-    device.turbo_mode = s1.turbo_mode
-    
-    # await device.apply()
-    await asyncio.sleep(1)
-    await device.refresh()
-    return status()
+@app.post('/{device}/bulk')
+async def parameter_data(device, s1: ac_config):
+    cfg = get_config(device)
+    if cfg == "Provided config not found in config file":
+        return ("Device Name not found")
+    # Processing Midea 
+    if cfg.get("modul") == "midea":
+        # build midea device
+        device = AC(ip=cfg.get("device_ip"), port=int(cfg.get("device_port")), device_id=int((cfg.get("device_id"))))
+        # authenticate to device
+        await device.authenticate(cfg.get("device_token"), cfg.get("device_key"))
+        # get device status
+        await device.refresh()
+        # set operational_mode
+        device.power_state = s1.power_state
+        device.target_temperature = s1.target_temperature
+        device.operational_mode = s1.operational_mode
+        device.fan_speed = s1.fan_speed
+        device.swing_mode = s1.swing_mode
+        device.eco_mode = s1.eco_mode
+        device.turbo_mode = s1.turbo_mode
+        # apply status to device
+        await device.apply()
+        await asyncio.sleep(1)
+         # build status dict.
+        values =  {
+        'id': device.id,
+        'ip': device.ip,
+        'online': device.online,
+        'supported': device.supported,
+        'power_state': device.power_state,
+        'beep': device.beep,
+        'target_temperature': device.target_temperature,
+        'operational_mode': repr(device.operational_mode),
+        'fan_speed': repr(device.fan_speed),
+        'swing_mode': repr(device.swing_mode),
+        'eco_mode': device.eco_mode,
+        'turbo_mode': device.turbo_mode,
+        'fahrenheit': device.fahrenheit,
+        'indoor_temperature': device.indoor_temperature,
+        'outdoor_temperature': device.outdoor_temperature
+        }
+        return values 
